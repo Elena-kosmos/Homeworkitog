3d5598880c862b9b30db59bc6492b91843d986a0
Lena <eutereshkova@mail.ru>
2022-10-19 09:23:05 +0300

    добавили задачу

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿void metodArray(string[] array)
+ {
+    string[]newarr = new string[array.Length];
+    for (int i = 0; i <array.Length; i++)
+    {
+       if(array[i].Length<=3) 
+       {
+        newarr[i]=array[i];
+       Console.WriteLine(newarr[i]);
+       }
+    }
+        Console.WriteLine();
+ }
+ void Print(string[]Array)
+ {
+    for (int i = 0; i <Array.Length; i++)
+    {
+      Console.WriteLine(Array[i]);
+    }
+    Console.WriteLine();
+ }
+ string [] array = {"hello","2", "world",":-)"};
+ metodArray(array);
+ Print(array);
+
